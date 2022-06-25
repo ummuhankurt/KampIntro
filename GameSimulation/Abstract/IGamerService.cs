b0c1ed5d08c6d36952c5659b578061d9a92714c4
ummuhan kurt <ummuhankurt.23@outlook.com>
2022-06-25 11:57:23 +0300

    Game Simulation.

---
+++ GameSimulation/Abstract/IGamerService.cs
@@ -0,0 +1,17 @@
+﻿using GameSimulation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSimulation.Abstract
+{
+    public interface IGamerService
+    {
+        void Add(Gamer gamer);
+        void Update(Gamer gamer);
+        void Delete(Gamer gamer);
+
+    }
+}
---
+++ GameSimulation/Concrete/OfferManager.cs
@@ -0,0 +1,27 @@
+﻿using GameSimulation.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSimulation.Concrete
+{
+    public class OfferManager : IOfferService
+    {
+        public void AddOffer()
+        {
+            Console.WriteLine("Offer added");
+        }
+
+        public void DeleteOfer()
+        {
+            Console.WriteLine("Offer deleted");
+        }
+
+        public void UpdateOffer()
+        {
+            Console.WriteLine("Offer updated");
+        }
+    }
+}

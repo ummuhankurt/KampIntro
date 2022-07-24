2fa7709f120ae10ca6a94d44a1b4b44eda5343be
ummuhan kurt <ummuhankurt.23@outlook.com>
2022-07-24 14:33:16 +0300

    object oriented programming example 2.

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -6,6 +6,7 @@ namespace OOP2
     {
         static void Main(string[] args)
         {
+
             GercekMusteri musteri1 = new GercekMusteri();
             musteri1.Id = 1;
             musteri1.MusteriNo = "12345";

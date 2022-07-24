2b76c6c7dfaf78ed24cd15b3709275e00f265ba9
ummuhan kurt <ummuhankurt.23@outlook.com>
2022-07-24 14:01:44 +0300

    KampIntro. First commit

--- AOPDemo/ConsoleUI/Program.cs
+++ AOPDemo/ConsoleUI/Program.cs
@@ -14,6 +14,7 @@ namespace AOPDemo.ConsoleUI
     {
         static void Main(string[] args)
         {
+            // console ui.
             IProductService productService = new ProductManager(new EfProductDal());
             foreach (var item in productService.GetAll())
             {

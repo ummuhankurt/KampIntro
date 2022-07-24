91fbf32fc797667d17ed8f6859e75887f11d2720
ummuhan kurt <ummuhankurt.23@outlook.com>
2022-07-24 14:06:04 +0300

    Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,6 +6,7 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            // Arrays.
             string[] students = new string[] { "Derin","Engin","Salih"};
             foreach (var student in students)
             {
